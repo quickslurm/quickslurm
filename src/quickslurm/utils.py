9ee750c5f61190db7560e639de3a0ed33007d894
Jack <jack.quimby54@gmail.com>
2025-10-29 11:20:07 -0400

    revert

--- src/quickslurm/utils.py
+++ src/quickslurm/utils.py
@@ -135,7 +135,7 @@ def _slurm_wait(job_id) -> None:
 
             sleep(10)
 
-        except Exception as e:
+        except CalledProcessError as e:
             print(f'Failed to check slurm status: {e}')
             sleep(10)
 
